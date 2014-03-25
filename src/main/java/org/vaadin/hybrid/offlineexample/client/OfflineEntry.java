17446b874cb9501de63e9d3b974488e9506bcb3d
Joonas Lehtinen <joonas@vaadin.com>
2014-03-25 01:50:40 -0700

    Caching resourced for offline viewing with cache manifest

--- src/main/java/org/vaadin/hybrid/offlineexample/client/OfflineEntry.java
+++ src/main/java/org/vaadin/hybrid/offlineexample/client/OfflineEntry.java
@@ -1,25 +1,29 @@
 package org.vaadin.hybrid.offlineexample.client;
 
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.vaadin.client.ApplicationConfiguration;
 
 public class OfflineEntry extends ApplicationConfiguration {
 
-	@Override
-	public void onModuleLoad() {
+    @Override
+    public void onModuleLoad() {
 
         // Offline version is selected by going to /offline.html
-		if (OfflineRedirector.onOfflinePage()) {
+        if (OfflineRedirector.onOfflinePage()) {
 
             // Start offline by placing the AddressbookEditor on rootPanel
             RootPanel rootPanel = RootPanel.get("offlineui");
             rootPanel.add(new AddressbookEditor());
-		} else {
-			// Start online version
-			super.onModuleLoad();
-		}
+        } else {
 
-	}
+            // When we are offline, we should always try to load offline page
+            if (OfflineDetector.isOffline()) {
+                OfflineRedirector.redirectToOffline();
+            }
+
+            // Start online version
+            super.onModuleLoad();
+        }
+
+    }
 }
